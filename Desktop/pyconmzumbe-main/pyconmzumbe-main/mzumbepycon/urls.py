c80feced04f8e1c63c804c73a9dcd1545c8aadbb
DeveloperBhin <norbeth559@gmail,com>
2025-02-27 19:49:52 +0300

    i commit

--- Desktop/pyconmzumbe-main/pyconmzumbe-main/mzumbepycon/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.homepage, name='homepage'),
-    path('about/', views.about, name='about'),
-    path('register/',views.register,name='register')
-]
---
+++ mzumbepycon/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+LEVEL_CHOICES= [
+    ('Beginner', 'Beginner'),
+    ('Intermediate', 'Intermediate'),
+    ('Advanced', 'Advanced'),
+]
+ 
+
+class RegisterUser(models.Model):
+        fullname=models.CharField(max_length=30)
+        Course=models.CharField(max_length=30)
+        year=models.CharField(max_length=4)
+        Experience=models.CharField(max_length=30,choices=LEVEL_CHOICES,default='Begginer')
+        Phone=models.CharField(max_length=15)
+        Email=models.EmailField(max_length=30)
+        Student_ID = models.CharField(max_length=12)
+        
+def __str__(self):
+    return self.fullname
+    
